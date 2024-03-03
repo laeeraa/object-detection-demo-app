2bcd54c67dbe93feed7977760b947503f73c2e6e
lara <lara@Ubuntu.myguest.virtualbox.org>
2024-03-03 20:48:33 +0100

    resize to full screen

--- app/app.py
+++ app/app.py
@@ -13,19 +13,14 @@ if current_working_directory not in sys.path:
 
 from app.classes.MainWindow import MainWindow
 
-user32 = ctypes.windll.user32
-user32.SetProcessDPIAware()
-[w, h] = [user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)]
-
-# for Apple
-# import AppKit
-# [(screen.frame().size.width, screen.frame().size.height)
-#     for screen in AppKit.NSScreen.screen
-
 
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
+
+    availableSize = app.desktop().availableGeometry().size()
+    w = availableSize.width()
+    h = availableSize.height()
 
     # the code to start to create and start a timer every 100 milliseconds.
     # This way we can safely terminate our application with Ctrl-C from the command line.
@@ -34,7 +29,9 @@ if __name__ == "__main__":
     timer.start(100)
 
     win = MainWindow()
-    win.resize(w, h - 50)
+    #resize the Window to fill full screen
+    win.adjustSize()
+    win.resize(w, h)
     win.show()
 
     sys.exit(app.exec())
